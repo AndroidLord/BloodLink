2cf65c33c3c446eacce988f674da9bd97e6aed1c
ShubhamSinghBisht <shubham8318461615@bbdec.ac.in>
2023-03-16 13:33:54 +0530

    1) Connecting LoginActivity to LoginActivity.loginButton -> drawerNavigation LoginActivity.signUpText -> CreateActivity.class
    2) Assigning new names to Views in CreateActivity

--- app/src/main/java/com/example/bloodlink/LoginActivity.java
+++ app/src/main/java/com/example/bloodlink/LoginActivity.java
@@ -6,35 +6,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Button loginButton,createButton;
+    private Button loginButton;
+
+    private TextView signUpText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // ID's
+        loginButton = findViewById(R.id.loginButton);
+        signUpText = findViewById(R.id.signupText);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                //startActivity(new Intent(LoginActivity.this,));
+                startActivity(new Intent(LoginActivity.this, DrawerActivity.class));
 
             }
         });
 
-        createButton.setOnClickListener(new View.OnClickListener() {
+        signUpText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                startActivity(new Intent(LoginActivity.this,CreateActivity.class));
-
+                startActivity(new Intent(LoginActivity.this, CreateActivity.class));
             }
         });
-
 
     }
 }
