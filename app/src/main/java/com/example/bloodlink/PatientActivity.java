42231fe51b7586fa5c35a6da555c5d2d6b2b9ac3
ShubhamSinghBisht <shubham8318461615@bbdec.ac.in>
2023-03-18 17:08:37 +0530

    Testing Git

--- app/src/main/java/com/example/bloodlink/PatientActivity.java
+++ app/src/main/java/com/example/bloodlink/PatientActivity.java
@@ -11,5 +11,5 @@ public class PatientActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient);
     }
-
+    // made a comment
 }
